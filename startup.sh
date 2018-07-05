256deaea2447b741f4cfe134dcdf6611b25aacdc
lynchmaniac <lynchmaniac@gmail.com>
2018-07-05 19:09:14 +0200

    feat: add automatic launch

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Start my node project
+cd ~/dev/lea/leaspeaking && npm start
+# Print the IP address
+#_IP=$(hostname -I) || true
+#if [ "$_IP" ]; then
+#    printf "My IP address is %s\n" "$_IP"
+#fi
